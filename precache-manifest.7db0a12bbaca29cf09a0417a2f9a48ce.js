a34211bf80c903e0b9cb5522ebd99327a4e9d817
Jiho Park <bapjoo2@gmail.com>
2018-11-22 00:45:40 +0900

    Updates

---
+++ precache-manifest.7db0a12bbaca29cf09a0417a2f9a48ce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "52177ba3d99ca219bd49",
+    "url": "/burger_builder/static/css/3.b4037b7d.chunk.css"
+  },
+  {
+    "revision": "e7a8d0036c4dcdd87bdf",
+    "url": "/burger_builder/static/css/main.22220fcd.chunk.css"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "016ad159dc6f316d8c81",
+    "url": "/burger_builder/static/js/1.016ad159.chunk.js"
+  },
+  {
+    "revision": "fca93643c4feae191315",
+    "url": "/burger_builder/static/js/runtime~main.fca93643.js"
+  },
+  {
+    "revision": "6494738995631364bdca",
+    "url": "/burger_builder/static/js/2.64947389.chunk.js"
+  },
+  {
+    "revision": "e7a8d0036c4dcdd87bdf",
+    "url": "/burger_builder/static/js/main.e7a8d003.chunk.js"
+  },
+  {
+    "revision": "52177ba3d99ca219bd49",
+    "url": "/burger_builder/static/js/3.52177ba3.chunk.js"
+  },
+  {
+    "revision": "f7c3482be671148ea7c9",
+    "url": "/burger_builder/static/js/4.f7c3482b.chunk.js"
+  },
+  {
+    "revision": "6494738995631364bdca",
+    "url": "/burger_builder/static/css/2.da158e63.chunk.css"
+  },
+  {
+    "revision": "016ad159dc6f316d8c81",
+    "url": "/burger_builder/static/css/1.3e4ea3e9.chunk.css"
+  },
+  {
+    "revision": "2030c1b4e13d77cafac108da3b1ee595",
+    "url": "/burger_builder/index.html"
+  }
+];
